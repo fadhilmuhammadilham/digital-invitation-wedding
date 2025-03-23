3ca6e811ad2d158fca5ba66ae6eadfd6e07ac561
fadhilmuhammadilham <fadhilmuhammadilham29@gmail.com>
2025-03-24 03:45:04 +0700

    add projek digital invitation wedding

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#b18b64", // Coklat keemasan
+        secondary: "#e5d3b3", // Beige lembut
+        accent: "#8a5a44", // Coklat tua
+        background: "#f8f4f0", // Putih krem
+        border: "#c9a78b", // Coklat muda
+      },
+    },
+  },
+  plugins: [],
+}
+
